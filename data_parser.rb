7e2d4c24668b6b7969b76a7e03fb358205cdd114
lacewaves <Caroline@Carolines-MacBook-Air.local>
2015-09-11 14:14:38 -0500

    Yay - pilot totals added

--- data_parser.rb
+++ data_parser.rb
@@ -9,12 +9,44 @@ CSV.foreach("./planet_express_logs.csv", headers: true) do |row|
   destinations << row.to_hash
 end
 
-revenue = []
-CSV.foreach("./planet_express_logs.csv", headers: true) do |col|
-    revenue << [col[3]].reduce(:+).to_i
-end
+# revenue = []
+# CSV.foreach("./planet_express_logs.csv", headers: true) do |col|
+#     revenue << col[3].to_i
+# end
+
+revenue = destinations.map {|destination| destination["Money"].to_i }
 
 total_revenue = revenue.reduce(:+)
+
+bender = []
+fry = []
+amy = []
+leela = []
+
+destinations.each do |shipment|
+  if shipment["Destination"] == "Uranus"
+    bender << shipment
+
+  elsif shipment["Destination"] == "Earth"
+    fry << shipment
+
+  elsif shipment["Destination"] == "Mars"
+    amy << shipment
+
+  else
+    leela << shipment
+  end
+end
+
+bender_total = bender.map {|destination| destination["Money"].to_i }.reduce(:+)
+fry_total = fry.map {|destination| destination["Money"].to_i }.reduce(:+)
+amy_total = amy.map {|destination| destination["Money"].to_i }.reduce(:+)
+leela_total = leela.map {|destination| destination["Money"].to_i }.reduce(:+)
+
+bender_bonus = bender_total * 0.1
+fry_bonus = fry_total * 0.1
+amy_bonus = amy_total * 0.1
+leela_bonus = leela_total * 0.1
 
 b = binding
 html = File.read("./report.erb")
